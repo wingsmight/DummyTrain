0db2ba3d4cfd4bb95df347db22cea97432a6f04a
wingsmight <wingsmight@gmail.com>
2022-10-13 08:29:39 +0700

    Initial Commit

---
+++ DummyTrain/DummyTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyTrainApp.swift
+//  DummyTrain
+//
+//  Created by Igoryok on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DummyTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
